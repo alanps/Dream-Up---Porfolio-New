f30dd03507e54b313f8cd92c5fd72323da7b962e
Alan PS <alanps2012@gmail.com>
2022-05-14 08:45:59 -0300

    performance

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,6 +1,6 @@
 @include('sections.header')
 
-  <main id="main" class="main">
+  <main id="main" class="main" style="background-image: url(@asset('/images/bg.jpg'));">
     <div class="container404">
       <x-alert type="warning">
         <div class="cod">404</div>
