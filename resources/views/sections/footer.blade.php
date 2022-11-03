8578fd42a2223452daba8880ed601dc5c15fbe66
Alan PS <alanps2012@gmail.com>
2022-11-02 21:38:59 -0300

    adicionando facebook nas redes sociais

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -17,7 +17,7 @@
             <span class="meulinkedin">linkedin.com/in/alanpardinisantana</span>
           </a>
           <a class="facebook" href="https://web.facebook.com/alanps2012" target="_blank">
-            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-facebook-square"></i>
             <span class="meufacebook">web.facebook.com/alanps2012</span>
           </a>
         </div>
