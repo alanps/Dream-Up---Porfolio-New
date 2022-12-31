cb31add471e97e9b62cab37515a9e6548ee07021
Alan PS <alanps2012@gmail.com>
2022-12-31 10:09:01 -0300

    Ajustar bg attach smartphone, correções footer e ajuste foto curriculum

--- resources/scripts/geral.js
+++ resources/scripts/geral.js
@@ -12,3 +12,20 @@ jQuery('body').on('click', 'a', function(e) {
 		return;
 	}
 });
+
+jQuery('body').on("scroll", function(){
+	var offset = jQuery('body').scrollTop();
+	var header = jQuery('header').height();
+
+    if(jQuery('html').width() >= 300 && jQuery('html').width() <= 565){
+        jQuery('.bloco-eu').css('background-position' , 'center '+(((offset - header)/2.5 )-50)+'px');
+        jQuery('.bloco-curriculum').css('background-position' , 'center '+(((offset - header)/10 )+50)+'px');
+    } else if(jQuery('html').width() >= 768 && jQuery('html').width() <= 1024){
+        jQuery('.bloco-eu').css('background-position' , 'center '+(((offset - header)/2.5 )-320)+'px');
+        jQuery('.bloco-curriculum').css('background-position' , 'center '+(((offset - header)/10 )-40)+'px');
+    } else {
+        jQuery('.bloco-eu').css('background-position' , 'bottom');
+        jQuery('.bloco-curriculum').css('background-position' , 'top');
+    }
+
+});
