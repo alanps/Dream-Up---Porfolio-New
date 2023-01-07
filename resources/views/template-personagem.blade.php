6718833d257bb4b4ecf0254b7aa8cf03368ecc6a
Alan PS <alanps2012@gmail.com>
2023-01-07 08:29:29 -0300

    personagem: corrigindo template personagem

--- resources/views/template-personagem.blade.php
+++ resources/views/template-personagem.blade.php
@@ -843,7 +843,6 @@ $query = mysqli_query($bd, "SELECT * FROM `artes101` WHERE `grupo` LIKE '".$row[
 while ($row = mysqli_fetch_array($query))
 {
 echo "<a href=\"".$imgs.$row['imagem']."\" class=\"fancybox-button2\" rel=\"fancybox-".$row['grupo']."\"><img border=\"0\" src=\"".$imgs.$row['imagem']."\" class=\"bfotos2\"></a>";
-}
 
 ?>
 
@@ -864,6 +863,8 @@ jQuery('#btextocamiseta<?php echo $row['id']; ?>').slimScroll({
 </script>
 
 <?php
+
+}
 
 }
 
