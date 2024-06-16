880000dd6cc683015d0b2f7382001cc1a3b0b0f4
Alan PS <alanps2012@gmail.com>
2024-06-15 22:35:03 -0300

    build

--- wp-content/themes/agenciadreamup/.stylelintrc.js
+++ wp-content/themes/agenciadreamup/.stylelintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   ],
   rules: {
     'color-no-invalid-hex': true,
-    'indentation': 'tab',
-    'selector-class-pattern': ['^([a-z][a-z0-9]*)(-[a-z0-9]+)*$', '^([a-z][a-z0-9]*)(_[a-z0-9]+)*$']
+    'indentation': 'tab'
   },
 }
